5c4502aef0b043d92196261f89ccb68d3831c582
Amir Bawab <amirbawab@gmail.com>
2017-09-01 20:04:29 -0400

    Added a new problem

---
+++ ID_7.java
@@ -0,0 +1,46 @@
+class Prob {
+    public static void main(String[] args) {
+        Prob p = new Prob();
+        System.out.println(p.reverse(-123));
+        System.out.println(p.reverse(-0));
+        System.out.println(p.reverse(-321));
+        System.out.println(p.reverse(Integer.MAX_VALUE));
+        System.out.println(p.reverse(Integer.MIN_VALUE));
+        System.out.println(p.reverse(1534236469));
+    }
+
+    public int reverse(int num) {
+
+        // Calculate the inverse
+        int sign = num < 0 ? -1 : 1;
+        int cache = Math.abs(num);
+        int result = 0;
+        int prev = 0;
+        while(cache > 0) {
+
+            // Cache value
+            prev = result;
+            result *= 10;
+            
+            // Check multiplication overflow
+            if(result / 10 != prev) {
+                return 0;
+            }
+
+            // Get last digit to the right
+            int digit = cache % 10;
+            result += digit;
+            cache/=10;
+            
+            // Check addition overflow
+            if(result < 0) {
+                return 0;
+            }
+        }
+
+        // Recover the right sign
+        result *= sign;
+
+        return result;
+    }
+}
--- Readme.md
+++ Readme.md
@@ -5,3 +5,4 @@
 * [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/description/) - [Solution](ID_2.java)
 * [Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/description/) - [Solution](ID_3.java)
 * [ZigZag Conversion ](https://leetcode.com/problems/zigzag-conversion/description/) - [Solution](ID_6.java)
+* [Reverse Integer](https://leetcode.com/problems/reverse-integer/description/) - [Solution](ID_7.java)
